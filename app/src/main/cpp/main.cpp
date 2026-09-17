bcde2fba38711756afe0c30bf5addab1eabca334
SONICXX2011 <email@example.com>
2026-09-17 21:27:52 +0330

    Update project

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -1,6 +1,10 @@
 #include <jni.h>
 #include <android/log.h>
+
+extern "C" {
 #include <shadowhook.h>
+}
+
 #include "BNM/Loading.hpp"
 
 #define LOG_TAG "MyMod"
@@ -9,24 +13,20 @@
 void OnBNMLoaded() {
     LOGI("[+] BNM loaded successfully");
     LOGI("[+] Mod is ready!");
-    // اینجا کد Hook خودت رو بنویس
 }
 
-JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
+extern "C" JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved) {
     LOGI("[+] JNI_OnLoad");
 
-    // ✅ اول JNIEnv رو بگیر
     JNIEnv* env = nullptr;
     if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK) {
         LOGI("[+] Failed to get JNIEnv");
         return JNI_VERSION_1_6;
     }
 
-    // ۱. ShadowHook
     int result = shadowhook_init(SHADOWHOOK_MODE_UNIQUE, false);
     LOGI("[+] ShadowHook init: %d", result);
 
-    // ۲. BNM با JNIEnv
     BNM::Loading::AddOnLoadedEvent(OnBNMLoaded);
     BNM::Loading::TryLoadByJNI(env);
 
